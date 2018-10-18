cca3f51636d69a6c0721ad1d8bd7646591c93206
Taimur Shah <tshah@scu.edu>
2018-10-18 02:47:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function sayHiToGrandma (string) {
     return "I can't hear you!"; 
   }
   if (string.toUpperCase()===string) {
-    return
+    return "YES INDEED!";
   }
-
-  
-  
+  if (string==="I love you, Grandma.") {
+    return "I love you, too.";
+  }
 }
